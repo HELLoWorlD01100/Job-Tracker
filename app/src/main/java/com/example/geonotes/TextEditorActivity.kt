22d1e58e17cd218efb1d4f5288dc26eab7ce2b11
Popostatopulos <egorpostovalov@gmail.com>
2021-12-10 11:50:14 +0500

    simple text editor

--- app/src/main/java/com/example/geonotes/TextEditorActivity.kt
+++ app/src/main/java/com/example/geonotes/TextEditorActivity.kt
@@ -3,9 +3,10 @@ package com.example.geonotes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TextEditorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_text_editor)
+        setSupportActionBar(findViewById(R.id.teditor_toolbar))
     }
 }
